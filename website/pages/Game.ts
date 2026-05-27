31aaca18701a0759c937fd4f241affc627ac404d
_ <_>
2026-05-28 01:21:33 +0300

    workshop

--- website/pages/Game.ts
+++ website/pages/Game.ts
@@ -5,21 +5,26 @@ var BigJam = get("div", "jamDiv");
 var points = get("div", "points");
 var sound = get("audio", "click-sound");
 
-var score = 0;
+let score: number = 0;
+
 var token = localStorage.getItem("userToken");
 
-BigJam.addEventListener('click', () => {
-    sound.currentTime = 0; 
+BigJam.addEventListener('click', (): void => {
+    sound.volume = 0.55;
+    sound.currentTime = 0;
     sound.play(); 
-
-    score++;
-    points.innerText = "Jams: " + score;
+    void updateScore(1);
 });
 
+async function updateScore(add: number) {
+    if (!token) {
+        console.error("User is not authenticated.");
+        return;
+    }
 
-async function UpdateScore(add: number) {
-    
     score += add;
+
     points.innerText = "Jams: " + score;
-    send("addScore", token, add);
+
+    await send("addScore", token, add); 
 }
